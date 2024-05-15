b1e8ecd63cc7df497b79ef5f461ae0da9437c653
umer-farooq1784 <umerfarroq987@gmail.com>
2024-05-16 00:58:50 +0500

    fix: Strings names and images for career roadmap

--- LearningApp/app/build.gradle.kts
+++ LearningApp/app/build.gradle.kts
@@ -57,5 +57,5 @@ dependencies {
     implementation("com.cloudinary:cloudinary-android:2.5.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
 }
