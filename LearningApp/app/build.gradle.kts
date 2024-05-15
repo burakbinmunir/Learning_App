161a5517dcc49d65bf7d44194abadd54dd23af07
shehryar-munir <shehryar.munir@dubizzlelabs.com>
2024-05-16 01:47:53 +0500

    1) Added Maths & Arabic tests

--- LearningApp/app/build.gradle.kts
+++ LearningApp/app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.firebase.database)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
